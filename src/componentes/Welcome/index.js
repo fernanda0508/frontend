3310af73ef97df82485b6aefa4364fed164c0f04
fernanda0508 <fefecoelho0508@gmail.com>
2021-10-27 21:23:57 -0300

    mandando pro git

---
+++ src/componentes/Welcome/index.js
@@ -0,0 +1,13 @@
+import React from 'reat';
+function Welcome(){
+
+    return(
+        <div>
+        <h1>Hello Word</h1>
+        <h1>Hello Word</h1>
+        </div>
+    );
+
+}
+
+export default Welcome;
